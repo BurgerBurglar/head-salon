ee412c3a435ca057167b63a0aa0f8d5ba7994158
Shuo Tian <tianshuo1996@outlook.com>
2023-02-04 19:33:11 -0600

    comment CSS adjust

--- src/components/posts/Comment.tsx
+++ src/components/posts/Comment.tsx
@@ -6,7 +6,7 @@ const Comment: React.FC<Comment> = ({ username, date, content, replies }) => {
   return (
     <div className="border-b pb-3 text-left last-of-type:border-b-0">
       <div>
-        <span className="text-lg font-semibold">{username}</span>
+        <span className="font-semibold">{username}</span>
         <span className="ml-2 text-slate-500">{date}</span>
       </div>
       <div>{content}</div>
