e8f8951e705b2b9e862f4cce319d19f1886f41c4
Shuo Tian <tianshuo1996@outlook.com>
2023-02-04 19:49:14 -0600

    updated front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,16 @@
-import { type GetStaticProps } from "next";
-import { getNumPosts, getPosts } from "../scraper/fetch";
-import Posts from "./blogs/[page]";
+import { type NextPage } from "next";
 
-const Home = Posts;
-
-export const getStaticProps: GetStaticProps = async () => {
-  const posts = await getPosts(1);
-  const numPosts = await getNumPosts();
-  return {
-    props: {
-      posts,
-      numPosts,
-    },
-  };
+const Home: NextPage = () => {
+  return (
+    <>
+      <main className="article">
+        <h2>博主介绍</h2>
+        <p>
+          辉格，男，七十年代初生于江南；母语：吴语；职业：程序员（1993-2007），商业与时事评论（2008至今）。现居澳大利亚。
+        </p>
+      </main>
+    </>
+  );
 };
 
 export default Home;
