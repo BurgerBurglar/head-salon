b8567c7b6701adb3c6bd5700aca1bece56dc222d
Shuo Tian <tianshuo1996@outlook.com>
2023-02-08 20:55:27 -0600

    adjust pagination css

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -18,7 +18,7 @@ const Pagination: React.FC<PaginationProps> = ({
 
   return (
     <ReactPaginate
-      className="mx-auto flex w-fit gap-2 font-bold text-slate-600"
+      className="mx-auto mt-8 flex w-fit gap-2 font-bold text-slate-600"
       pageLinkClassName="grid h-[2em] w-[2em] place-items-center rounded-md hover:bg-slate-200"
       activeLinkClassName="bg-pink-500 text-white hover:bg-pink-500"
       previousLinkClassName="grid h-[2em] w-[2em] place-items-center rounded-md hover:bg-slate-200"
