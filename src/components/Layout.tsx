7bddbddfb906efb2b6d0b284e827fc10bbc80788
Shuo Tian <tianshuo1996@outlook.com>
2023-02-09 23:25:04 -0600

    fix build error by case sensitivity

---
+++ src/components/Layout.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import React, { type PropsWithChildren } from "react";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>海德沙龙 (HeadSalon)</title>
+        <meta name="description" content="Blog by WhigZhou" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <div className="mx-auto mt-[68px] w-full max-w-3xl flex-1 p-3 pb-12">
+        {children}
+      </div>
+      <Footer />
+    </>
+  );
+};
+export default Layout;
