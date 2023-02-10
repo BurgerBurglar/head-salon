38c01ac47f4b17c07f2c71d51f46ea143f8a1dab
Shuo Tian <tianshuo1996@outlook.com>
2023-02-09 18:12:05 -0600

    add loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type AppType } from "next/dist/shared/lib/utils";
 import Layout from "../components/Layout";
+import NextNProgress from "nextjs-progressbar";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ import "../styles/globals.scss";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <QueryClientProvider client={queryClient}>
+      <NextNProgress color="rgb(219 39 119)" />
       <Layout>
         <Component {...pageProps} />
       </Layout>
